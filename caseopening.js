127d10d835c5d8e6be269773d7ba1211104ff964
Daniel Sehr <daniel@sehr.se>
2023-04-07 05:03:06 +0200

    all weapons added, some fixes, all working

--- caseopening.js
+++ caseopening.js
@@ -1,29 +1,37 @@
-var items = {
-    blue: { //79.92%
-        chance: { min: 1, max: 7988 },
-        skin: "P90 | Grim",
-        img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_p90_cu_p90_grimm_light_large.59514d7b54f637f314329bb3fef1c7ffd1b153ba.png"
-    },
-    purple: { //15.98%
-        chance: { min: 7989, max: 9587 },
-        skin: "Desert Eagle | Directive",
-        img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_deagle_aq_desert_eagle_constable_light_large.fb2f2673dd3997a21bff9129e0d2e294c03095e8.png"
-    },
-    pink: { //3.20%
-        chance: { min: 9588, max: 9908 },
-        skin: "Tec-9 | Fuel Injector",
-        img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_tec9_gs_tec9_supercharged_light_large.e3ebc20f10eae02790fe95703b6099acca1d1809.png"
-    },
-    red: { //0.64%
-        chance: { min: 9909, max: 9973 },
-        skin: "AK-47 | Neon Revolution",
-        img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_ak47_cu_ak47_anarchy_light_large.6e98f0c6fcb81aaeca03c56eed68962f50c9ef94.png"
-    },
-    yellow: { //0.26%
-        chance: { min: 9974, max: 10000 },
-        skin: "Karambit | Gamma Doppler",
-        img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_knife_karambit_am_gamma_doppler_phase1_light_large.769cf2ab676ea2a7d7322c258f57bac8dca00336.png"
-    }
+var gamma2;
+fetch('./JSON/gamma2case.json')
+    .then((response) => response.json())
+    .then((json) => gamma2 = json);
+
+// var items = {
+//     blue: { //79.92%
+//         skin: "P90 | Grim",
+//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_p90_cu_p90_grimm_light_large.59514d7b54f637f314329bb3fef1c7ffd1b153ba.png"
+//     },
+//     purple: { //15.98%
+//         skin: "Desert Eagle | Directive",
+//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_deagle_aq_desert_eagle_constable_light_large.fb2f2673dd3997a21bff9129e0d2e294c03095e8.png"
+//     },
+//     pink: { //3.20%
+//         skin: "Tec-9 | Fuel Injector",
+//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_tec9_gs_tec9_supercharged_light_large.e3ebc20f10eae02790fe95703b6099acca1d1809.png"
+//     },
+//     red: { //0.64%
+//         skin: "AK-47 | Neon Revolution",
+//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_ak47_cu_ak47_anarchy_light_large.6e98f0c6fcb81aaeca03c56eed68962f50c9ef94.png"
+//     },
+//     yellow: { //0.26%
+//         skin: "Karambit | Gamma Doppler",
+//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_knife_karambit_am_gamma_doppler_phase1_light_large.769cf2ab676ea2a7d7322c258f57bac8dca00336.png"
+//     }
+// };
+
+var drop_chance = {
+    blue: { min: 1, max: 7988 },
+    purple: { min: 7989, max: 9587 },
+    pink: { min: 9588, max: 9908 },
+    red: { min: 9909, max: 9973 },
+    yellow: { min: 9974, max: 10000 },
 };
 
 var quality = {
@@ -32,13 +40,18 @@ var quality = {
     FT: { min: 1787, max: 6100 },
     MW: { min: 6101, max: 8560 },
     FN: { min: 8561, max: 10000 },
-}
+};
 
 var tempCounter = 0;
 
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomInArray(array) {
+    return Math.floor(Math.random() * array.length);
+    // return array[Math.floor(Math.random() * array.length)];
 }
 
 function getQuality() {
@@ -53,15 +66,21 @@ function getQuality() {
 
 function getStatTrak() {
     var rand = randomInt(1, 10)
-    if (rand > 5) {
+    if (rand == 1) {
         var stattrak = '<div class="stattrak_class">StatTrak&trade;</div>';
         return stattrak
     }
 }
 
+function resetCards() {
+    for (i = 0; i < 90; i++) {
+        $('#CardNumber' + i).remove();
+    }
+}
+
 function resetInventory() {
     for (var i = 0; i < 7; i++) {
-        document.getElementById("win_item" + i).remove();
+        document.getElementById("added_inventory" + i).remove();
         tempCounter = 0;
     }
 }
@@ -70,21 +89,26 @@ function GenerateDrop() {
     $('.slots').css({ // resets position of wheel
         transition: "sdf",
         "margin-left": "0px"
-    }, 10).html('');
+    });
 
+    resetCards()
     for (var i = 0; i < 90; i++) {
-        var element;
         var rand = randomInt(1, 10000);
-        for (x in items) {
-            if (rand >= items[x].chance.min && rand <= items[x].chance.max) {
-                element = '<div id="CardNumber' + i + '" class="item ' + x + '_item_color" style="background-image:url(' + items[x].img + ');"></div>';
+
+        for (x in drop_chance) {
+            if (rand >= (drop_chance[x].min) && rand <= (drop_chance[x].max)) {
+                var random_drop = randomInArray(gamma2[x])
+                var skin_name = gamma2[x][random_drop][0]
+                var skin_image = gamma2[x][random_drop][1]
+                element = '<div id="CardNumber' + i + '" class="item ' + x + '_item_color" style="background-image:url(' + skin_image + ');"></div>';
                 if (i === 77) {
-                    var winningSkin = items[x].skin
-                    var winningImage = items[x].img
+                    var winningSkin = skin_name
+                    var winningImage = skin_image
                     var winningColor = x
                 }
             }
         }
+
         $(element).appendTo('.slots');
     }
 
@@ -104,13 +128,14 @@ function OpenCase(skinName, skinImage, skinColor) {
         }
 
         $('#CardNumber77').addClass('winning-item');
-        var win_element = "<div id='win_item" + tempCounter + "' class='item " + skinColor + "_item_color' style='background-image: url(" + skinImage + ")'></div>";
+        var win_element = "<div id='added_inventory" + tempCounter + "' class='item " + skinColor + "_item_color' style='background-image: url(" + skinImage + ")'></div>";
         $(win_element).appendTo('.inventory');
         var quality = getQuality()
-        $(quality).appendTo('#win_item' + tempCounter);
+        $(quality).appendTo('#added_inventory' + tempCounter);
         var stattrak = getStatTrak()
-        $(stattrak).appendTo('#win_item' + tempCounter);
+        $(stattrak).appendTo('#added_inventory' + tempCounter);
         tempCounter += 1;
-    }, 500);
+    }, 8500);
+    // $('.slots').css('margin-left', -(tempCounter + 1) * 6770 + 'px');
     $('.slots').css('margin-left', '-6770px');
 }
