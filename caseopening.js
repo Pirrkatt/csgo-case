d8897d17527b2e70994049b3aa9dc137c31cf2d4
Daniel Sehr <daniel@sehr.se>
2023-04-09 08:09:17 +0200

    fixed money spent text removal, button fade-out, re-made calculations for less lag, remade code & general cleanup - mostly works!

--- caseopening.js
+++ caseopening.js
@@ -1,30 +1,18 @@
-var gamma2;
+var gamma2; // LOCALHOST
 fetch('./JSON/gamma2case.json')
     .then((response) => response.json())
     .then((json) => gamma2 = json);
 
-// var items = {
-//     blue: { //79.92%
-//         skin: "P90 | Grim",
-//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_p90_cu_p90_grimm_light_large.59514d7b54f637f314329bb3fef1c7ffd1b153ba.png"
-//     },
-//     purple: { //15.98%
-//         skin: "Desert Eagle | Directive",
-//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_deagle_aq_desert_eagle_constable_light_large.fb2f2673dd3997a21bff9129e0d2e294c03095e8.png"
-//     },
-//     pink: { //3.20%
-//         skin: "Tec-9 | Fuel Injector",
-//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_tec9_gs_tec9_supercharged_light_large.e3ebc20f10eae02790fe95703b6099acca1d1809.png"
-//     },
-//     red: { //0.64%
-//         skin: "AK-47 | Neon Revolution",
-//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_ak47_cu_ak47_anarchy_light_large.6e98f0c6fcb81aaeca03c56eed68962f50c9ef94.png"
-//     },
-//     yellow: { //0.26%
-//         skin: "Karambit | Gamma Doppler",
-//         img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_knife_karambit_am_gamma_doppler_phase1_light_large.769cf2ab676ea2a7d7322c258f57bac8dca00336.png"
-//     }
-// };
+// https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=StatTrak%E2%84%A2%20M4A1-S%20|%20Hyper%20Beast%20(Minimal%20Wear)
+// $(window).on('load', function() {
+//     $.getJSON('./JSON/gamma2case.json', function(json) {
+//         gamma2 = json
+//     })
+
+//     $.getJSON('https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=StatTrak%E2%84%A2%20M4A1-S%20|%20Hyper%20Beast%20(Minimal%20Wear)', function(data) {
+//         console.log(data);
+//     });
+// });
 
 var drop_chance = {
     blue: { min: 1, max: 7988 },
@@ -72,82 +60,89 @@ function getStatTrak() {
     }
 }
 
-function resetCards() {
-    for (i = 0; i < 90; i++) {
-        $('#CardNumber' + i).remove();
-    }
-}
-
-function resetInventory() {
-    for (var i = 0; i < 7; i++) {
-        document.getElementById("added_inventory" + i).remove();
-        tempCounter = 0;
-    }
-}
-
 var simulateKnife = false
-var simulations = 1
+var currently_rolling = false
+var simulationCount = 1
 var keyPrice = 2.5
 
 function simulateKnifeToggle() {
     return simulateKnife = document.getElementById("switchToggle").checked;
 }
 
-function simText(param) {
-    // console.log($('.simulating-text').css("visibility")
-    $('.simulating-text').css({
-        "visibility": param
-    });
-    $('.lds-roller').css({
-        "visibility": param
-    });
+function moneySpent(param) {
+    if (param === "add") {
+        var string = "Total Cases Opened for a Knife: <span class='color-white'>" + simulationCount + "</span>"
+        string = string + "<br>" + "Total Money Spent on Keys:  <span class='color-green'>" + (simulationCount * keyPrice) + "$</span>"
+        string = string + "<br>" + "Knife Value: <span class='color-red'>" + "Unknown</span>"
+        var moneyText = '<div class="money-spent-text">' + string + '</div>';
+        return moneyText
+    }
+    if (param === "remove") {
+        $('.money-spent-text').remove();
+    }
 }
 
-function getMoneySpent() {
-    var string = "Total Cases Opened for a Knife: <span class='color-white'>" + simulations + "</span>"
-    string = string + "<br>" + "Total Money Spent on Keys:  <span class='color-green'>" + (simulations * keyPrice) + "$</span>"
-    string = string + "<br>" + "Knife Value: <span class='color-red'>" + "Unknown</span>"
-    var moneyText = '<div class="money-spent-text">' + string + '</div>';
-    return moneyText
+function hideButton() {
+    var button = $('.roll-button')
+    button.css({ cursor: "default", opacity: 0, transition: "opacity 1s" })
+    currently_rolling = true
+
+    setTimeout(function() {
+        button.css({ cursor: "pointer", opacity: 1, transition: "opacity 1s" })
+        currently_rolling = false
+    }, 9200);
 }
 
-function GenerateDrop() {
+function clickOpen() {
+    if (currently_rolling) {
+        return
+    }
+
     $('.slots').css({ // resets position of wheel
         transition: "sdf",
         "margin-left": "0px"
     });
+    hideButton()
+    moneySpent("remove")
+    return GenerateDrop()
+}
 
-    resetCards()
-    if (simulateKnife) {
-        simText("visible")
+var random_array = [];
+
+function GenerateDrop() {
+    for (var i = 0; i < 90; i++) { // resets cards
+        $('#CardNumber' + i).remove();
     }
+
     for (var i = 0; i < 90; i++) {
         var rand = randomInt(1, 10000);
-
-        for (x in drop_chance) {
-            if (rand >= (drop_chance[x].min) && rand <= (drop_chance[x].max)) {
-                var random_drop = randomInArray(gamma2[x])
-                var skin_name = gamma2[x][random_drop][0]
-                var skin_image = gamma2[x][random_drop][1]
-                element = '<div id="CardNumber' + i + '" class="item ' + x + '_item_color" style="background-image:url(' + skin_image + ');"></div>';
-                if (i === 77) {
-                    var winningSkin = skin_name
-                    var winningImage = skin_image
-                    var winningColor = x
-
-                    if (simulateKnife) {
-                        //show simulating text...
-                        if (!(rand >= 9974)) {
-                            simulations += 1
-                            return GenerateDrop()
-                        } else {
-                            $(getMoneySpent()).appendTo('.sim-container');
-                        }
-                    }
+        random_array[i] = rand
+        if (simulateKnife) {
+            if (i === 77) {
+                if (!(rand >= drop_chance.yellow.min)) {
+                    simulationCount += 1
+                    return GenerateDrop() // call function again if winning item is not yellow (knife)
+                } else {
+                    $(moneySpent("add")).appendTo('.sim-container');
                 }
             }
         }
+    }
 
+    for (var z = 0; z < 90; z++) {
+        for (x in drop_chance) {
+            if (random_array[z] >= (drop_chance[x].min) && random_array[z] <= (drop_chance[x].max)) {
+                var random_drop = randomInArray(gamma2[x])
+                var skin_name = gamma2[x][random_drop][0]
+                var skin_image = gamma2[x][random_drop][1]
+                element = '<div id="CardNumber' + z + '" class="item ' + x + '_item_color" style="background-image:url(' + skin_image + ');"></div>';
+                if (z === 77) {
+                    var winningSkin = skin_name
+                    var winningImage = skin_image
+                    var winningColor = x
+                }
+            }
+        }
         $(element).appendTo('.slots');
     }
 
@@ -160,21 +155,21 @@ function OpenCase(skinName, skinImage, skinColor) {
     $('.slots').css({
         transition: "all 8s cubic-bezier(.08,.6,0,1)"
     });
-    if (simulateKnife) {
-        simText("hidden")
-    }
+
     setTimeout(function() {
         if (tempCounter >= 7) {
-            resetInventory()
+            for (var i = 0; i < 7; i++) { // resets inventory
+                document.getElementById("added_inventory" + i).remove();
+                tempCounter = 0;
+            }
         }
 
-        $('#CardNumber77').addClass('winning-item');
+        $('#CardNumber77').addClass('winning-item'); // green border on winning item
         var win_element = "<div id='added_inventory" + tempCounter + "' class='item " + skinColor + "_item_color' style='background-image: url(" + skinImage + ")'></div>";
-        $(win_element).appendTo('.inventory');
+        $(win_element).appendTo('.inventory'); //adds winning item to inventory
         $(getQuality()).appendTo('#added_inventory' + tempCounter);
         $(getStatTrak()).appendTo('#added_inventory' + tempCounter);
         tempCounter += 1;
     }, 8500);
-    // $('.slots').css('margin-left', -(tempCounter + 1) * 6770 + 'px');
     $('.slots').css('margin-left', '-6770px');
 }
